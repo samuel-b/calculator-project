b3a2c53b5b76bdff7a9f3ec4e6ee6dd6c7b899cb
Sam-B-Hub <64763981+Sam-B-Hub@users.noreply.github.com>
2022-03-26 18:18:11 +1100

    added clear functionality and validation

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const display = document.querySelector(".calculator__display");
 const numberDecimalButtons = document.querySelectorAll(".button__number");
 const operatorButtons = document.querySelectorAll(".button__operator");
-const equalButton = document.querySelector(".button__equal")
+const equalButton = document.querySelector(".button__equal");
+const clearButton = document.querySelector(".button__clear");
+
 let previousValue = 0;
 let currentValue = 0;
 
 for (let i = 0; i < numberDecimalButtons.length; i++) {
     numberDecimalButtons[i].addEventListener("click", (e) => {
-        display.innerText += e.target.innerText;
+        if (display.innerText.length < 15) {
+            display.innerText += e.target.innerText;
+        }
     });
 }
 
 for (let i = 0; i < operatorButtons.length; i++) {
     operatorButtons[i].addEventListener("click", (e) => {
+        const hasNumber = /[0-9]/;
         previousValue = Number(display.innerHTML);
-        display.innerText += e.target.innerText;
+
+        if (
+            display.innerText.length < 15 &&
+            hasNumber.test(display.innerText)
+        ) {
+            display.innerText += e.target.innerText;
+        }
     });
 }
 
@@ -40,7 +51,13 @@ equalButton.addEventListener("click", () => {
             break;
 
         default:
-            console.log("There is an issue");
+            display.innerHTML = "TRY USING ONE OPERATOR AT A TIME";
             break;
     }
+});
+
+clearButton.addEventListener("click", () => {
+    display.innerText = "";
+    previousValue = 0;
+    currentValue = 0;
 });
